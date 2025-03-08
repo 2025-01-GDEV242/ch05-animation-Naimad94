dc7a6e8d24f8c4cb3391668edeb64b7a7aa494a0
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2025-03-07 19:23:38 -0500

    finished

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,7 @@
 import java.awt.Color;
+import java.util.Random; //import random package.
+import java.util.ArrayList; //import array package.
+import java.lang.Math; //import math package.
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,6 +14,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private ArrayList<BoxBall> boxBallList;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -43,9 +47,10 @@ public class BallDemo
         boolean finished =  false;
         while (!finished) 
         {
-            myCanvas.wait(50);           // small delay
+            myCanvas.wait(50); //delay
             ball.move();
             ball2.move();
+            
             // stop once ball has travelled a certain distance on x axis
             if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) 
             {
@@ -54,15 +59,108 @@ public class BallDemo
         }
     }
     
-    //
-    public void boxBounce()
+    /**
+     * This method simulates a random number of bouncing balls
+     * The balls will bounce for two minutes before freezing in place
+     * @param howMany How many balls are in the box
+     */
+    public void boxBounce(int howMany)
     {
-        myCanvas.setVisible(true); //turning on the canvas.
+        myCanvas.setVisible(true);
         
-        int ground = 400; //position of the ground line.
-        int roof = 100; //position of the roof line.
-        int leftWall = 50; //Position of the left wall.
+        // define the box offsets:
+        int ground = 400;
+        int leftWall = 50;
+        int rightWall = 550;
+        int ceiling = 100;
         
-        drawBox(ground, ceiling, leftWall, rightWall); //draw the box.
+        // draw the box
+        drawBox(ground, ceiling, leftWall, rightWall);
+
+        //create an ArrayList of BoxBalls:
+        boxBallList = new ArrayList<BoxBall>();
+        int index = 0;
+        while(index < howMany)
+        {
+            //create a randomizer to aid ball creation
+            Random ballCreator = new Random();
+            
+            // define parameters for the ball
+            
+            // ball size randomly between 10 and 60 pixels:
+            int size = ballCreator.nextInt(51) + 10;
+            
+            // x and y start positions randomly within the box
+            int xRange = rightWall - leftWall - size/2;
+            int xStart = ballCreator.nextInt(xRange) + leftWall;
+            int yRange = ground - ceiling - size/2;
+            int yStart = ballCreator.nextInt(yRange) + ceiling;
+            
+            // ball color randomly chosen (RGB values only allowed up to
+            // 230, to account for visibility on white canvas)
+            int rVal = ballCreator.nextInt(231);
+            int gVal = ballCreator.nextInt(231);
+            int bVal = ballCreator.nextInt(231);
+            
+            Color ballColor = new Color(rVal, gVal, bVal);
+            
+            // ball speeds randomly between 1 and 7:
+            int horizontalSpeed = ballCreator.nextInt(7) + 1;
+            int verticalSpeed = ballCreator.nextInt(7) + 1;
+            
+            BoxBall currentBall = new BoxBall(xStart, yStart, size, 
+                ballColor, ground, ceiling, leftWall, rightWall, 
+                myCanvas, horizontalSpeed, verticalSpeed);
+            
+            boxBallList.add(currentBall);
+            index++;
+        }
+        
+        // make the balls bounce
+        long startTime = System.currentTimeMillis();
+        long endTime;
+        boolean finished =  false;
+        while (!finished) 
+        {
+            myCanvas.wait(50);           // small delay
+            // move each ball
+            for(BoxBall ball : boxBallList)
+            {
+                //ball.draw(); // for testing ball positioning
+                ball.move();
+                
+                // re-draw the box
+                drawBox(ground, ceiling, leftWall, rightWall);
+            }
+            endTime = System.currentTimeMillis();
+            long timeDifference = Math.subtractExact(endTime, startTime);
+            // stop the simulation after 2 minutes
+            if (timeDifference >= 120000)
+            {
+                finished = true;
+            }
+        }
     }
+    
+    /**
+     * This is a helper method for drawing a box on the Canvas
+     * @param bottom The y- coordinate of the bottom of the box
+     * @param top The y-coordinate of the top of the box
+     * @param left The x-coordinate of the left side of the box
+     * @param right The x-coordinate of the right side of the box
+     */
+    private void drawBox(int bottom, int top, int left, int right)
+    {
+        // draw the box
+        myCanvas.setForegroundColor(Color.BLACK);
+        // ground:
+        myCanvas.drawLine(left, bottom, right, bottom);
+        // left wall:
+        myCanvas.drawLine(left, bottom, left, top);
+        // right wall:
+        myCanvas.drawLine(right, bottom, right, top);
+        // ceiling:
+        myCanvas.drawLine(left, top, right, top);
+    }
+    
 }
--- BoxBall.java
+++ BoxBall.java
@@ -1,7 +1,7 @@
-import java.awt.Color;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Write a description of class BoxBall here.
@@ -11,36 +11,129 @@ import java.awt.geom.*;
  */
 public class BoxBall
 {
-    // instance variables - replace the example below with your own
+    private Ellipse2D.Double circle;
     private Color color;
+    private int diameter;
     private int xPosition;
     private int yPosition;
-    private final int groundPosition;      // y position of ground
+    private final int groundPosition;       // y position of ground
     private final int ceilingPosition;      // y position of ceiling
     private final int leftWallPosition;     // x position of left wall
     private final int rightWallPosition;    // x position of right wall
     private Canvas canvas;
+    private int xSpeed;                   // initial horizontal speed
+    private int ySpeed;                   // initial vertical speed
 
     /**
      * Constructor for objects of class BoxBall
+     *
+     * @param xPos  the horizontal coordinate of the ball
+     * @param yPos  the vertical coordinate of the ball
+     * @param ballDiameter  the diameter (in pixels) of the ball
+     * @param ballColor  the color of the ball
+     * @param groundPos  the position of the ground (where the wall will bounce)
+     * @param drawingCanvas  the canvas to draw this ball on
      */
-    public BoxBall(int xPos, int yPos, int groundPos, int ceilingPos, int leftWallPos, int rightWallPos, 
-    Canvas drawingCanvas)
+    
+    public BoxBall(int xPos, int yPos, int ballDiameter, Color ballColor,
+                        int groundPos, int ceilingPos, int leftPos, 
+                        int rightPos, Canvas drawingCanvas, int xSpd,
+                        int ySpd)
     {
+        // initialise instance variables
         xPosition = xPos;
         yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
         groundPosition = groundPos;
         ceilingPosition = ceilingPos;
-        leftWallPosition = leftWallPos;
-        rightWallPosition = rightWallPos;
+        leftWallPosition = leftPos;
+        rightWallPosition = rightPos;
         canvas = drawingCanvas;
+        xSpeed = xSpd;
+        ySpeed = ySpd;
     }
-    
+
     /**
      * Draw this ball at its current position onto the canvas.
      **/
     public void draw()
     {
         canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
     }
+    //erases ball.
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+        
+        Random speedModifier = new Random();
+        if(xSpeed > 0)
+        {
+            xPosition += (speedModifier.nextInt(7) + 1);
+        }
+        else
+        {
+            xPosition -= (speedModifier.nextInt(7) + 1);
+        }
+        
+        if(ySpeed > 0)
+        {
+            yPosition += (speedModifier.nextInt(7) + 1);
+        }
+        else
+        {
+            yPosition -= (speedModifier.nextInt(7) + 1);
+        }
+        
+        // made for bouncing.
+        if(yPosition >= (groundPosition - diameter))
+        {
+            yPosition = groundPosition - diameter;
+            ySpeed = -1 * ySpeed;
+        }
+        else if(yPosition <= ceilingPosition)
+        {
+            yPosition = ceilingPosition;
+            ySpeed = -1 * ySpeed;
+        }
+        else if(xPosition >= (rightWallPosition - diameter))
+        {
+            xPosition = rightWallPosition - diameter;
+            xSpeed = -1 * xSpeed;
+        }
+        else if(xPosition <= leftWallPosition)
+        {
+            xPosition = leftWallPosition;
+            xSpeed = -1 * xSpeed;
+        }
+
+        // draw in a new location.
+        draw();
+    }  
+    
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    } 
 }
